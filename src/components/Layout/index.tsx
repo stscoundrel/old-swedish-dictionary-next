6708f5af381ce3d4b4c6b4f4f87b5bc54801a049
Sampo Silvennoinen <silvennoinen.sampo@gmail.com>
2022-11-13 09:55:03 +0200

    Layour component

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,15 +1,37 @@
 // Components.
 import Head from 'components/Head'
-import Footer from 'components/Footer'
+// import Footer from 'components/Footer'
+import Breadcrumbs from 'components/Breadcrumbs'
+import Navigation from 'components/Navigation'
+// import BackToTop from 'components/BackToTop'
+import { ContentType } from 'lib/models/content-types'
+import { DictionaryEntry } from 'lib/models/dictionary'
+import { AlphabetLetter } from 'lib/services/dictionary'
 
-export default function Layout({ children }) {
+interface LayoutProps{
+  type: ContentType,
+  word: DictionaryEntry | null,
+  words: DictionaryEntry[],
+  letters: AlphabetLetter[],
+  letter: AlphabetLetter | null,
+  children: JSX.Element | JSX.Element[],
+}
+
+export default function Layout({
+  type, letters, word = null, words = [], children, letter = null,
+}: LayoutProps) {
   return (
     <>
-      <Head></Head>
+      <Head type={type} word={word} words={words} letter={letter} />
+      <header>
+        <Navigation letters={letters} />
+        <Breadcrumbs word={word} letter={letter} />
+      </header>
       <main className="container">
         {children}
+        {/* <BackToTop /> */}
       </main>
-      <Footer/>
+      {/* <Footer letters={letters}/> */}
     </>
   )
 }
