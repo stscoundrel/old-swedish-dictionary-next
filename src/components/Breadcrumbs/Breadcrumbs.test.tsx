8f918536239fdc650eee81fe95f5066143bfa5bd
Sampo Silvennoinen <silvennoinen.sampo@gmail.com>
2022-11-13 09:06:19 +0200

    Add breadcrumbs component

---
+++ src/components/Breadcrumbs/Breadcrumbs.test.tsx
@@ -0,0 +1,77 @@
+import ReactDOM from 'react-dom/client'
+import renderer from 'react-test-renderer'
+import Breadcrumbs from './index'
+
+describe('Breadcrumbs component', () => {
+  const word = {
+    headword: 'afköra',
+    partOfSpeech: 'vb',
+    grammaticalAspect: 'v.',
+    definitions: [
+      ' , vräka. hafwi wald han afköra  GS 43 (1416?). af ty ahren (för areno) han landboen afkörer ib.',
+    ],
+    alternativeForms: ['-bortdrifva'],
+    slug: 'afkora',
+  }
+
+  const letter = {
+    letter: 'a',
+    slug: 'a',
+  }
+
+  test('Default page: does not crach', () => {
+    const div = document.createElement('div')
+    const root = ReactDOM.createRoot(div)
+    root.render(<Breadcrumbs word={null} letter={null} />)
+  })
+
+  test('Default page: matches snapshot', () => {
+    const tree = renderer.create(<Breadcrumbs word={null} letter={null} />).toJSON()
+    expect(tree).toMatchSnapshot()
+  })
+
+  test('Default page: renders correct amount of breadcrumbs', () => {
+    const tree = renderer.create(<Breadcrumbs word={null} letter={null} />)
+    const { root } = tree
+
+    expect(root.findAllByType('a').length).toEqual(1)
+  })
+
+  test('Letter page: does not crach', () => {
+    const div = document.createElement('div')
+    const root = ReactDOM.createRoot(div)
+    root.render(<Breadcrumbs word={null} letter={letter} />)
+  })
+
+  test('Letter page: matches snapshot', () => {
+    const tree = renderer.create(
+      <Breadcrumbs word={null} letter={letter} />,
+    ).toJSON()
+    expect(tree).toMatchSnapshot()
+  })
+
+  test('Letter page: renders correct amount of breadcrumbs', () => {
+    const tree = renderer.create(<Breadcrumbs word={null} letter={letter} />)
+    const { root } = tree
+
+    expect(root.findAllByType('a').length).toEqual(2)
+  })
+
+  test('Word page: does not crach', () => {
+    const div = document.createElement('div')
+    const root = ReactDOM.createRoot(div)
+    root.render(<Breadcrumbs word={word} letter={letter} />)
+  })
+
+  test('Word page: matches snapshot', () => {
+    const tree = renderer.create(<Breadcrumbs word={word} letter={letter} />).toJSON()
+    expect(tree).toMatchSnapshot()
+  })
+
+  test('Word page: renders correct amount of breadcrumbs', () => {
+    const tree = renderer.create(<Breadcrumbs word={word} letter={letter} />)
+    const { root } = tree
+
+    expect(root.findAllByType('a').length).toEqual(3)
+  })
+})
---
+++ src/components/Breadcrumbs/index.tsx
@@ -0,0 +1,47 @@
+import { BreadcrumbRequest, getBreadcrumbs } from 'lib/utils/breadcrumbs'
+import { getBreadcrumbsSchema } from 'lib/services/schema'
+import Link from 'next/link'
+import { DictionaryEntry } from 'lib/models/dictionary'
+import { AlphabetLetter } from 'lib/services/dictionary'
+import styles from './Breadcrumbs.module.scss'
+
+interface BreadcrumbProps{
+  word: DictionaryEntry | null,
+  letter: AlphabetLetter | null
+}
+
+export default function Breadcrumbs({ word, letter }: BreadcrumbProps) {
+  const getBreadCrumbsRequest = (): BreadcrumbRequest => {
+    const request: BreadcrumbRequest = {
+      word: null,
+      letter: null,
+    }
+
+    if (word) {
+      request.word = word.headword
+    }
+
+    if (letter) {
+      request.letter = letter.letter
+    }
+
+    return request
+  }
+
+  const breadcrumbs = getBreadcrumbs(getBreadCrumbsRequest())
+  const schema = getBreadcrumbsSchema(breadcrumbs)
+
+  return (
+    <nav className={styles.section}>
+      <div className="container">
+        {breadcrumbs.map(({ label, url }) => (
+          <Link key={url} href={url} className={styles.link}>
+            {label}
+          </Link>
+        ))}
+
+        <script type='application/ld+json' dangerouslySetInnerHTML={ { __html: schema } }/>
+      </div>
+    </nav>
+  )
+}
