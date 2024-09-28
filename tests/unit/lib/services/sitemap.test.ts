855feda2bc2f5a67bd7871ddcd875c27a86d51cd
Sampo Silvennoinen <silvennoinen.sampo@gmail.com>
2024-09-28 17:19:31 +0300

    Guard sitemap freshness with test asserts

--- tests/unit/lib/services/sitemap.test.ts
+++ tests/unit/lib/services/sitemap.test.ts
@@ -1,12 +1,22 @@
 import { createSitemap } from 'lib/services/sitemap'
+import { readFileSync } from 'node:fs'
 
 describe('Sitemap tests', () => {
-  process.env.NEXT_PUBLIC_SITE_URL = 'https://old-swedish-dictionary.test'
+  process.env.NEXT_PUBLIC_SITE_URL = 'https://old-swedish-dictionary.vercel.app'
 
   test('Sitemap content can be formatted to XML.', async () => {
     const result = await createSitemap()
 
     expect(result.includes('<?xml version="1.0" encoding="UTF-8"?>')).toBeTruthy()
-    expect(result.includes('<url><loc>https://old-swedish-dictionary.test/word/aremark</loc><changefreq>monthly</changefreq><priority>0.8</priority></url>')).toBeTruthy()
+    expect(result.includes('<url><loc>https://old-swedish-dictionary.vercel.app/word/aremark</loc><changefreq>monthly</changefreq><priority>0.8</priority></url>')).toBeTruthy()
+  })
+
+  test('Generated sitemap in public is up to date.', async () => {
+    // Whenever underlying data changes, the sitemap should change too.
+    // Guard this with comparison to freshly generated version.
+    const publicSitemap = readFileSync(`${__dirname}/../../../../public/sitemap.xml`).toString()
+    const result = await createSitemap()
+
+    expect(publicSitemap).toEqual(result)
   })
 })
