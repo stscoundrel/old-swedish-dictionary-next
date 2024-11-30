de4cc4cbcc2a33b7d6a12887a3b115f526c15b16
Sampo Silvennoinen <silvennoinen.sampo@gmail.com>
2024-11-30 09:57:30 +0200

    Slugs: add max length

--- src/lib/utils/slugs.ts
+++ src/lib/utils/slugs.ts
@@ -36,7 +36,17 @@ export const slugifyLetter = (letter: string): string => {
   return slug.toLowerCase()
 }
 
-export const slugifyWord = (word: string): string => slugify(word).toLowerCase()
+export const slugifyWord = (word: string): string => {
+  const result = slugify(word).toLowerCase();
+
+  if (result.length < 50) {
+    return result;
+  }
+
+  // Slugs longer than this are essentially data errors
+  // just clean them up to build passes.
+  return result.substring(0, 50);
+}
 
 export const decodeLetter = (slug: string): string => {
   let letter = ''
